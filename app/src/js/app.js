3f834640acb46c881006ed14851b1362eaab8d1a
Sergey Ivchenko <mr.jazzmix@gmail.com>
2020-02-21 17:00:22 +0200

    refactoring

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -3,27 +3,28 @@ import { ScrollScene, addIndicators } from 'scrollscene';
 import { gsap, Linear } from 'gsap';
 
 // Webp Support images
-!(function(e) {
-  function s(s) {
-    if (s) {
-      var t = e.documentElement;
-      t.classList ? t.classList.add("webp") : (t.className += " webp"),
-        window.sessionStorage.setItem("webpSupport", !0);
+(e => {
+  const isWebp = (webp) => {
+    if (webp) {
+      const webpImg = e.documentElement;
+      webpImg.classList ? webpImg.classList.add("webp") : webpImg.className += " webp",
+      window.sessionStorage.setItem("webpSupport", !false);
     }
-  }
-  !(function(e) {
-    if (window.sessionStorage && window.sessionStorage.getItem("webpSupport"))
-      s(!0);
+	};
+
+  (e => {
+    if (window.sessionStorage && window.sessionStorage.getItem("webpSupport")) {
+			isWebp(!false);
+		}
     else {
-      var t = new Image();
-      (t.onload = t.onerror = function() {
-        e(2 === t.height);
-      }),
-        (t.src =
-          "data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA");
+			const img = new Image();
+	
+      img.onload = img.onerror = ()  => e(2 === img.height),
+			img.src = "data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA";
     }
-  })(s);
+  })(isWebp);
 })(document);
+
 
 // Time Lines
 const Tl1 = gsap.timeline({ paused: true })
