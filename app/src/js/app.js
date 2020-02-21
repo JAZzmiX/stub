64ee5b5613b5b71dcf78ec76544110addd5f7ee3
Sergey Ivchenko <mr.jazzmix@gmail.com>
2020-02-21 14:50:36 +0200

    added webp support

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -2,7 +2,30 @@ import "../scss/style.scss";
 import { ScrollScene, addIndicators } from 'scrollscene';
 import { gsap, Linear } from 'gsap';
 
+// Webp Support images
+!(function(e) {
+  function s(s) {
+    if (s) {
+      var t = e.documentElement;
+      t.classList ? t.classList.add("webp") : (t.className += " webp"),
+        window.sessionStorage.setItem("webpSupport", !0);
+    }
+  }
+  !(function(e) {
+    if (window.sessionStorage && window.sessionStorage.getItem("webpSupport"))
+      s(!0);
+    else {
+      var t = new Image();
+      (t.onload = t.onerror = function() {
+        e(2 === t.height);
+      }),
+        (t.src =
+          "data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMwAgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA");
+    }
+  })(s);
+})(document);
 
+// Time Lines
 const Tl1 = gsap.timeline({ paused: true })
 	.to('.banner-1 .banner__img', 1, {y: "150%", ease: Linear.easeNone,  opacity: 0})
 	.to('.banner-1 .banner__img', 1, {y: "-150%", ease: Linear.easeNone,  opacity: 0});
